66b7ecb076bda01d4196313f150f27bb67e464f6
kamillysilva12 <kaamillysilvasantos@gmail.com>
2025-08-19 22:15:48 -0300

    trabalhando com estruturas de operações

--- operadores.php
+++ operadores.php
@@ -1,9 +1,27 @@
 <?php 
- echo("trabalhando com operadores em PHP <br />");
- echo("operadores de soma <br/>");
- echo("operadores de subtração <br/>");
- echo("operadores de multiplicação <br/>");
- echo("operadores de divisão <br/>");
+ echo("Trabalhando com operadores em PHP <br />");
+ echo("Operadores de soma <br/>");
+ echo("Operadores de subtração <br/>");
+ echo("Operadores de multiplicação <br/>");
+ echo("Operadores de divisão <br/>");
  
- echo("insira a operação desejada <br/>");
+ echo("Insira a operação desejada <br/>");
+	$num1 = 10;
+	$num2 = 2;
+
+	$resp =$num1 + $num2;
+	echo("A soma de $num1 e $num2 é: $resp <br/>");
+
+	$resp =$num1 - $num2;
+	echo("A subtração de $num1 e $num2 é: $resp <br/>");
+
+	$resp =$num1 * $num2;
+	echo("A multiplicação de $num1 e $num2 é: $resp <br/>");
+
+	$resp =$num1 / $num2;
+	echo("A divisão de $num1 e $num2 é: $resp <br/>");
+
+	$resp =$num1 % $num2;
+	echo("O resto da divisão de $num1 e $num2 é: $resp <br/>");
+
  ?>
