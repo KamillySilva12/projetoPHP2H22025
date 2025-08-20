f3c82700684dfdf1541f905e6e7f1153674b2f8c
kamillysilva12 <kaamillysilvasantos@gmail.com>
2025-08-19 21:16:30 -0300

    trabalhando com operadores e variáveis em php

---
+++ operadores.php
@@ -0,0 +1,9 @@
+<?php 
+ echo("trabalhando com operadores em PHP <br />");
+ echo("operadores de soma <br/>");
+ echo("operadores de subtração <br/>");
+ echo("operadores de multiplicação <br/>");
+ echo("operadores de divisão <br/>");
+ 
+ echo("insira a operação desejada <br/>");
+ ?>
