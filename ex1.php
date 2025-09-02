0616a305bc674e8d683b03e88d9c907fe70bea19
kamillysilva12 <kaamillysilvasantos@gmail.com>
2025-09-02 19:59:54 -0300

    Calculadora

--- ex1.php
+++ ex1.php
@@ -1,78 +1,79 @@
 <?php 
-function titulo(){
-	echo ("Calculadora: <br/>");
-}
 
-function soma($num1, $num2){
+//funções
 
-	return ($num1 + $num2);
+function soma($num1,$num2){
+	return $num1 + $num2;
 }
 
 function subtracao($num1, $num2){
-
-	return ($num1 - $num2);
+	return $num1 - $num2;
 }
 
 function divisao($num1, $num2){
-
-	return ($num1 / $num2);
+	// Verificar se o divisor é 0
+	if ($num2 == 0){
+		return false;
+	}
+	return $num1 / $num2;
 }
 
 function multiplicacao($num1, $num2){
-
-	return ($num1 * $num2);
+	return $num1 * $num2;
 }
 
-
-
-function menu(){
-
-	echo("A calculadora realiza as seguintes operações <br />");
-	echo("1- Soma <br />");
-    echo("2- Subtração <br />");
-    echo("3- Divisão <br />");
-    echo("4- Multiplicação <br />");
-    echo("5- Sair <br />");
-
-    echo ("Digite o número da operação desejada: ");
-    echo ("Digite o 1 número: ");
-    echo ("Digite o 2 número: ");
+function exibirMenu(){
+	echo "Calculadora <br /> <br />";
+	echo "Digite o número da operação desejada: <br /> <br />";
+	echo"1 - Soma <br />";
+	echo"2 - Subtração  <br />";
+	echo"3 - Divisão <br />";
+	echo"4 - Multiplicação <br /> <br />";
 }
 
-function verificarCalculo($operacao){
-	if($operacao == 1){
-		nome = "Soma";
-	}
-	elseif($operacao == 2){
-		nome = "Subtração";
-	}
-	elseif($operacao == 3){
-		nome = "Divisão";
-	}
-	elseif($operacao == 4){
-		nome = "Multiplicação";
-	}
+//Início do código
 
-	return nome;
+exibirMenu();
+
+$numero1 = 20;
+$numero2 = 0;
+$operacao = 3;
+
+switch ($operacao) {
+	case '1':
+		$resultado = soma($numero1, $numero2);
+		$nome_operacao = "Soma";
+		break;
+	case '2':
+		$resultado = subtracao($numero1, $numero2);
+		$nome_operacao = "Subtração";
+		break;
+	case '3':
+		$resultado = divisao($numero1, $numero2);
+		$nome_operacao = "Divisâo";
+		break;
+	case '4':
+		$resultado = multiplicacao($numero1, $numero2);
+		$nome_operacao = "Multiplicação";
+		break;
+	
+	default:
+		echo "ERRO: Operação inválida";
+		break;
 }
 
+//se o resultado  for um resultado falso, então mostre o motivo
+if ($resultado !== false){
+	echo "O resultado da $nome_operacao entre $numero1 e $numero2 é: $resultado";
+}
+else{
 
-
-$valor1 = 10; $valor2 = 2;
-$operacao = 1;
-
-
-
-titulo();
-menu();
-echo ("Os números digitados foram $valor1 e $valor2 <br />");
-$calculo = verificarCalculo();
-echo("O cálculo escolhido foi: $calculo <br />");
-
-//o cal escolhido foi
-// o resultado foi
+	echo "ERRO: Não é possível dividir por 0";
+}
 
 
  ?>
+
+
 
 
