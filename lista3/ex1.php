90fc43b554f00f89afd2a0b65cfaf07a4139b8db
kamillysilva12 <kaamillysilvasantos@gmail.com>
2025-09-30 20:58:36 -0300

    lista 2

---
+++ lista3/ex1.php
@@ -0,0 +1,18 @@
+<?php 
+//criar um sistema em php que calcule as somas das matrizes
+$matriz_a = array(
+     array (25, 12, 35),
+     array (85, 47, 98),
+     array (32, 38, 105));
+
+$matriz_b = array(
+     array (98, 65, 35),
+     array (5, 27, 8),
+     array (74, 14, 3));
+
+
+
+
+
+
+ ?>
---
+++ listaex/foreach_array_key.php
@@ -0,0 +1,9 @@
+<?php 
+
+$arr = array ('V' => 'Verde', 'e' => 'Verde', 'z' => 'Azul');
+
+foreach ($arr as $key => $value) {
+	echo $key . "=>" . $value . "\n";
+}
+
+ ?>
