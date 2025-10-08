5e94dcb292180670f25fafdb8d472da13411f50a
kamillysilva12 <kaamillysilvasantos@gmail.com>
2025-10-07 21:15:35 -0300

    Exercício 5

---
+++ lista3/ex5.php
@@ -0,0 +1,25 @@
+<?php 
+$cadastro = [
+	['nome' => 'Kamilly', 'cidade' => 'São Paulo', 'idade' => 19, 'sexo' => 'F'],
+	['nome' => 'Marcelly', 'cidade' => 'Santos', 'idade' => 21, 'sexo' => 'F'],
+	['nome' => 'Gabriela', 'cidade' => 'São Paulo', 'idade' => 19, 'sexo' => 'F'],
+	['nome' => 'Geovanna', 'cidade' => 'São Paulo', 'idade' => 18, 'sexo' => 'F'],
+	['nome' => 'Luana', 'cidade' => 'São Paulo', 'idade' => 17, 'sexo' => 'F'],
+
+	['nome' => 'Luan', 'cidade' => 'São Paulo', 'idade' => 19, 'sexo' => 'M'],
+	['nome' => 'João', 'cidade' => 'Santos', 'idade' => 17, 'sexo' => 'M'],
+	['nome' => 'Wilson', 'cidade' => 'São Paulo', 'idade' => 20, 'sexo' => 'M'],
+	['nome' => 'Lucas', 'cidade' => 'São Paulo', 'idade' => 22, 'sexo' => 'M'],
+	['nome' => 'Marcos', 'cidade' => 'Santos', 'idade' => 18, 'sexo' => 'M']
+];
+
+$contMasc = 0;
+
+echo"RESULTADO: <br/>"
+
+// nomes e idades 
+
+
+
+
+ ?>
